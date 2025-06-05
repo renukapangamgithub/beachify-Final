c77fd9bc1ba0334d9a30b506bfa31af8fa1895c2
Renuka Pangam <159953063+renukapangamgithub@users.noreply.github.com>
2025-06-05 12:36:29 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://beachify-final-backend.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
